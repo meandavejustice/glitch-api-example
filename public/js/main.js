8800a3ae75390f8b1dca1e1145de7ea889324458
David Adam Justice <david@outthinkgroup.com>
2014-08-21 12:09:39 -0700

    resubmit

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,26 @@
 var file = document.querySelector('input[type=file]');
 var submitBtn = document.querySelector('.submit')
+var resubmitBtn = document.querySelector('.resubmit')
 
 var img = document.createElement('img');
 
 var payload = {}
-
+var uploadedResult;
 
 var submitHandler = function(e){
   e.preventDefault()
   var xhr = new XMLHttpRequest()
+
+  // enable reglitching of images
+  if (img.src && e.target.classList.contains('resubmit')) {
+    payload.content = img.src;
+  } else {
+    payload.content = uploadedResult;
+  }
+
   xhr.onreadystatechange = function(){
     var parsed;
+    resubmitBtn.style.display = "inline";
     if (xhr.readyState === 4) {
       parsed = JSON.parse(xhr.responseText)
       img.src = parsed.content;
@@ -23,11 +33,12 @@ var submitHandler = function(e){
 
 var fileHandler = function(e) {
   var reader = new FileReader()
+
   reader.onloadend = function() {
+    resubmitBtn.style.display = "none";
     document.querySelector('header').appendChild(img);
+    uploadedResult = reader.result;
     img.src = reader.result
-    payload.content = reader.result
-    console.log(payload)
   }
 
   if (!~e.target.files[0].type.indexOf("image")) {
@@ -37,6 +48,6 @@ var fileHandler = function(e) {
   }
 }
 
-
 submitBtn.addEventListener('click', submitHandler);
+resubmitBtn.addEventListener('click', submitHandler);
 file.addEventListener('change', fileHandler);
